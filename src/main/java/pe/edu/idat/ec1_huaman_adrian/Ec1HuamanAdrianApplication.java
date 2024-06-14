2cb4671fbd0148b7934e73925321106a30bd5ca8
AHuamanChoque <adriansan734@gmail.com>
2024-06-13 21:22:56 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/ec1_huaman_adrian/Ec1HuamanAdrianApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1_huaman_adrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec1HuamanAdrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec1HuamanAdrianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/ec1_huaman_adrian/Ec1HuamanAdrianApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1_huaman_adrian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec1HuamanAdrianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
